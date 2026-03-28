cd415b82b7faa88dec570e47505f75b1b1e0511e
Akash <vangadaakash529@gmail.com>
2026-03-28 21:38:45 +0530

    Fix dynamic SSR issue

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
 import FilterSidebar from '@/components/FilterSidebar/FilterSidebar';
